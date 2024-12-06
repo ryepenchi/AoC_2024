b7521215e773921098754037e9be295af2bf80f6
Benjamin Reimitz <73063344+ryepenchi@users.noreply.github.com>
2024-12-06 16:24:36 +0100

    start day 6

--- day-6/solution.py
+++ day-6/solution.py
@@ -4,13 +4,35 @@ def main(filename):
 
     with open(filename, "r") as file:
         data = [line.strip() for line in file.readlines()]
-    print(data)
+        obstacles = []
+        guard = tuple()
+        guardstep = tuple()
+        for row, line in enumerate(data):
+            for column, character in enumerate(line):
+                if character == "#":
+                    obstacles.append((row, column))
+                elif character in ("^", "v", "<", ">"):
+                    guard = (row, column)
+                    if character == "^":
+                        guardstep = (-1, 0)
+                    elif character == "v":
+                        guardstep = (1, 0)
+                    elif character == "<":
+                        guardstep = (0, -1)
+                    elif character == ">":
+                        guardstep = (0, 1)
+
+    print(obstacles)
+    print(guard)
+    print(guardstep)
 
     part1(data)
     part2(data)
 
 def part1(data):
-    pass
+    
+    def gridstep(data):
+        pass
 
 def part2(data):
     pass
